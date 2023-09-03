2ea45518e98a1200c94ccc6e874f8d9bcc6290cd
Blaze Marshall <roboticblaze@gmail.com>
2023-09-03 18:04:13 -0400

    Basic Nav Bar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 // import { useState } from 'react'
-import './App.css'
 import RoutePaths from './routes/RoutePaths'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import { Box ,createTheme,ThemeProvider} from '@mui/material'
+import {blue} from '@mui/material/colors'
 function App() {
+  const theme= createTheme({
+    palette:{
+      primary:blue
+    },
+    transitions:{
+      
+    },
+    
+  })
+  // const color = red[500]
+  // console.log(color)
 
 
   return (
     <>
+    <ThemeProvider theme={theme}>
+
+
     <Header />
+    <Box  color='primary'      sx={{margin:"70px 0 0 0",}} >
+
        <Router>
         <RoutePaths />
        </Router>
+    </Box>
     <Footer />       
+       </ThemeProvider>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,72 @@
+import styled from '@emotion/styled'
+import { Box, Typography,Button, duration} from '@mui/material'
+import AppBar from '@mui/material/AppBar'
+import ToolBar from '@mui/material/Toolbar'
+import { createTheme,ThemeProvider } from '@mui/material/styles'
+// import makeStyles from '@mui/styles/makeStyles'
 
 const Header = () => {
+
+const btnTheme = createTheme({
+  transitions:{
+    navBtn:{
+      transitionDuration:'3s'
+    }
+
+  }
+
+});
+
+const buttonList = ['Culture','Safety','Policies','Benefits','Time-Off','Contact']
+const ButtonMap = buttonList.map((button,index)=>{
   return (
-    <>
-    <div>Header</div>
+    <Button key={index} 
+    href={`/${button.toLowerCase()}`}
+    sx={{
+      color:'white',
+      padding:'0 5px',
+      margin:'0 2.5px',
+      transition:'all 1s ease-in-out',
+      backgroundColor:'primary.main',
+      borderRadius:"50px",
+      '&:hover':{
+        backgroundColor:'white',
+        borderRadius:'2px',
+        color:'hotpink',
+        padding:'0px'
+
+    }
+      
+    }}
+   >
+    <ThemeProvider theme={btnTheme}>
+    {button}
+    </ThemeProvider>
+    </Button>)
+  })
+
+
+
+  return (
+    < >
+    <AppBar  >
+      <ToolBar sx={{justifyContent:'space-between'}} >
+        <Button variant='Contained' 
+                href='/'
+                >
+          Wahsega
+        </Button>
+        <Box sx={{
+          display:"flex",
+          padding:' 0 30px',
+          margin:'0px',
+          gap:'1px',
+          flexDirection:'row',
+        }}>
+          {ButtonMap}
+        </Box>
+      </ToolBar>
+    </AppBar>
     </>
   )
 }
